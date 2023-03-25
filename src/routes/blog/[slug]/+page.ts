67cd0299f302e1606612e46ece8ece18679aeffb
kwangjong <choikj0903@gmail.com>
2023-03-24 23:59:27 -0500

    fetch post from server

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,40 +1,27 @@
 // import { marked } from "marked";
 // import yaml from "js-yaml";
 
-interface Info {
-        layout: string,
-        title: string,
-        tags: string[],
-        date: Date,
+interface Post {
+    Url: string,
+    Title: string,
+    Date: Date,
+    Tags: string[],
+    Markdown: string,
+    Html: string,
 }
 
-export async function load({ fetch }) {
-    // let markdown: string = await fetch("https://raw.githubusercontent.com/kwangjong/kwangjong.github.io/main/_posts/blog/2023/02/2023-02-15-Go-Multi-threading.md")
-    //     .then((response: Response) => response.blob())
-    //     .then((blob: Blob) => blob.text())
-    //     .then((markdown: string) => {
-    //         return markdown;
-    //     });
-    
-    // let header: string = markdown.split("---")[1];
-    
-    // let parsed_markdown: string = marked.parse(markdown.split("---")[2]);
+export async function load({ fetch, params }) {
+    let post: Post = await fetch(`http://localhost:8080/blog/${params.slug}`)
+        .then((response: Response) => response.json());
 
-    // let info = yaml.load(header) as Info;
-
+    let date = new Date(post.Date)
     var options: any = { year: 'numeric', month: 'long', day: 'numeric' };
-    let date = new Date(Date.now())
     let dateString: string = date.toLocaleDateString("en-US", options);
     
     let content: string = `
-        <h1 class="title">Hello World</h1>
+        <h1 class="title">${post.Title}</h1>
         <time class="date" datetime="${date}" itemprop="datePublished">${dateString}</time>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <pre><code class="language-python">def hello():\n    print("hello world")\n\nhello()
-</code></pre>
+        ${post.Html}
         `;
-
-
-
     return { content } ;
   }
