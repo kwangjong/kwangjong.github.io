5e2cfef609fa994580a4978593237a6c7a52c7aa
kwangjong <choikj0903@gmail.com>
2025-01-26 23:06:20 +0900

    fix routing issue

---
+++ build/_app/immutable/chunks/navigation.7eeefda1.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.4350235e.js";const e=o("goto");export{e as g};
--- svelte.config.js
+++ svelte.config.js
@@ -24,7 +24,8 @@ const config = {
 		prerender: {
 			entries: [
 			  '/',
-			  '/blog'
+			  '/blog',
+			  '/new'
 			]
 		  }
 	}
