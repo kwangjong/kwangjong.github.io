ee1084fd4c7976bd9b436b8ad15fbf4b3b51f19f
kwangjong <choikj0903@gmail.com>
2025-01-27 22:42:55 +0900

    remove adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,3 @@
-//import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,12 +7,6 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-        adapter: adapter({
-			trailingSlash: 'never'
-		  }),
         paths: {
             base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
         },
