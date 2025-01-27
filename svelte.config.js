7597d7b0c0efdb18eca844c7d96a69d3e5db1632
kwangjong <choikj0903@gmail.com>
2025-01-27 23:07:16 +0900

    Revert "remove adapter-static"
    This reverts commit ee1084fd4c7976bd9b436b8ad15fbf4b3b51f19f.

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,5 @@
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,6 +9,12 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        adapter: adapter({
+			trailingSlash: 'never'
+		  }),
         paths: {
             base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
         },
